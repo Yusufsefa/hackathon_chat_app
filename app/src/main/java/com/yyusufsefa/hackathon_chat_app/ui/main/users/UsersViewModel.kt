426f33b5d3de80f0a69001bd552e010e464a3f16
Ramazan Öğünç <ramazanogunc@hotmail.com>
2021-10-16 12:57:15 +0300

    Add search in users fragment

--- app/src/main/java/com/yyusufsefa/hackathon_chat_app/ui/main/users/UsersViewModel.kt
+++ app/src/main/java/com/yyusufsefa/hackathon_chat_app/ui/main/users/UsersViewModel.kt
@@ -37,4 +37,37 @@ class UsersViewModel : ViewModel() {
             })
         }
     }
+
+    fun searchUserList(text: String) {
+        viewModelScope.launch {
+            if (text.isNotEmpty()) {
+                val dbRef = Firebase.database.getReference("users")
+                dbRef.addValueEventListener(object : ValueEventListener {
+                    override fun onDataChange(snapshot: DataSnapshot) {
+                        val userList = mutableListOf<User>()
+                        for (item in snapshot.children) {
+                            val user = item.getValue<User>()
+                            if (user != null &&
+                                user.userId != FirebaseAuth.getInstance().currentUser?.uid &&
+                                userConditionBySearchText(user, text)
+                            )
+                                userList.add(user)
+                        }
+                        _userList.value = userList.toList()
+                    }
+
+                    override fun onCancelled(error: DatabaseError) {}
+                })
+            } else {
+                fetchUserList()
+            }
+        }
+    }
+
+    private fun userConditionBySearchText(user: User, searchText: String): Boolean {
+        return user.name?.contains(
+            searchText,
+            ignoreCase = true
+        ) ?: false || user.lastName?.contains(searchText, ignoreCase = true) ?: false
+    }
 }
