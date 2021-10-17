16a9490ee66a18ce78bbdf797ec7e6ef2bbd1c2c
Ramazan Öğünç <ramazanogunc@hotmail.com>
2021-10-17 13:03:58 +0300

    Add validation login and register

--- app/src/main/java/com/yyusufsefa/hackathon_chat_app/util/Util.kt
+++ app/src/main/java/com/yyusufsefa/hackathon_chat_app/util/Util.kt
@@ -1,7 +1,10 @@
 package com.yyusufsefa.hackathon_chat_app.util
 
 import android.content.Context
+import androidx.core.widget.doOnTextChanged
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import com.google.android.material.textfield.TextInputEditText
+import com.google.android.material.textfield.TextInputLayout
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
@@ -29,4 +32,33 @@ fun saveCurrentUserDeviceToken(token: String) {
         val deviceToken = DeviceToken(token)
         dbRef.setValue(deviceToken)
     }
+}
+
+// check of textinput
+fun TextInputEditText.isNotNullOrEmpty(errorString: String): Boolean {
+    val textInputLayout = this.parent.parent as TextInputLayout
+    textInputLayout.errorIconDrawable = null
+    this.doOnTextChanged { _, _, _, _ ->
+        textInputLayout.error = null
+    }
+
+    return if (this.text.toString().trim().isEmpty()) {
+        textInputLayout.error = errorString
+        false
+    } else {
+        true
+    }
+}
+
+// çokul viewlerin validasyonunu sağlamak
+fun validateAndDo(textInputEditText: List<TextInputEditText>, block: () -> Unit) {
+
+    var boolean = false
+    for (text in textInputEditText) {
+        if (!text.isNotNullOrEmpty("Required"))
+            boolean = true
+    }
+
+    if (!boolean)
+        block()
 }
